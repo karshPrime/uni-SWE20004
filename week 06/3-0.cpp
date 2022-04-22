1e551de0d20565414df3c2c79edd093635f0ebd5
ut-kr <utkarsh@cyberfear.com>
2022-04-22 15:51:33 +1000

    🎁 task completed

---
+++ week 06/3-0.cpp
@@ -0,0 +1,7 @@
+int main() {
+  int arr[10]; // declaring array of size 10
+// by default all elements are set to 0
+// hence no point in declaring again with all elements as 0
+  arr[0] = 100; // overwriting first value as 100
+  arr[9] = 1000; // overwriting last value as 1000
+}
